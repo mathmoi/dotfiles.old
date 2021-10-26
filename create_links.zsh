ea3ff9fe7767e85edd0fd64f921543e5b331969e
Mathieu Pagé <m@mathieupage.com>
2021-10-26 00:23:05 -0400

    Added some configurations files for alacritty.

---
+++ create_links.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+ln -s ~/dotfiles/alacritty.yml ~/.alacritty.yml
+
+mkdir ~/.local
+mkdir ~/.local/share
+mkdir ~/.local/share/fonts
+ln -s ~/dotfiles/FSEX300.ttf ~/.local/share/fonts/FSEX300.ttf
